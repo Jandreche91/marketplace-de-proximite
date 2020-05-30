ccf458c51c3c82a56307432581a0252b309a233b
javier echeverri <jecheverrid@gmail.com>
2020-05-30 17:17:56 +0200

    phone number is now an integer, migration fixed the problem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_30_145035) do
+ActiveRecord::Schema.define(version: 2020_05_30_150939) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,8 +28,8 @@ ActiveRecord::Schema.define(version: 2020_05_30_145035) do
     t.string "last_name"
     t.string "address"
     t.string "city_name"
-    t.integer "city_postcode"
-    t.integer "phone_number"
+    t.string "city_postcode"
+    t.string "phone_number"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
