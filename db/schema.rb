91bb05c15b97897a82b40c8568fc1a60fae95ee7
javier echeverri <jecheverrid@gmail.com>
2020-06-04 19:33:00 +0200

    model booking created - relation and validations to be added in the model --> pending

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_02_182640) do
+ActiveRecord::Schema.define(version: 2020_06_04_172849) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "users_id", null: false
+    t.bigint "courgettes_id", null: false
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["courgettes_id"], name: "index_bookings_on_courgettes_id"
+    t.index ["users_id"], name: "index_bookings_on_users_id"
+  end
 
   create_table "courgettes", force: :cascade do |t|
     t.string "name"
@@ -44,5 +55,7 @@ ActiveRecord::Schema.define(version: 2020_06_02_182640) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "courgettes", column: "courgettes_id"
+  add_foreign_key "bookings", "users", column: "users_id"
   add_foreign_key "courgettes", "users"
 end
