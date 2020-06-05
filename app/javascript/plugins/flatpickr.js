a63deb2002e4f216093b33a62422e30a83a889d7
javier echeverri <jecheverrid@gmail.com>
2020-06-05 19:31:54 +0200

    validations of the form in JS advancing

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -5,3 +5,21 @@ flatpickr(".datepicker", {  altInput: true,
                             enableTime: false,
                             minDate: "today",
 });
+
+
+// validation that the end date must be greater than the start date
+
+const startDate = document.getElementById("booking_start_date");
+const endDate =  document.getElementById("booking_start_date");
+const form = document.getElementById("new_booking");
+
+form.addEventListener("change",(event) => {
+  console.log(startDate.value)
+  if ( Date.parse(startDate.value) > Date.parse(endDate.value)) {alert("End date should be greater than Start date")};
+});
+
+
+
+
+
+
