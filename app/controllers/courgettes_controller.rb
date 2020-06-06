74f55b81bf71cbd03139bd23b940b7b0fad39130
Li Brunel <li.cai@hotmail.fr>
2020-06-06 14:34:28 +0200

    version to delete

--- app/controllers/courgettes_controller.rb
+++ app/controllers/courgettes_controller.rb
@@ -10,7 +10,6 @@ class CourgettesController < ApplicationController
       {
         lat: user.latitude,
         lng: user.longitude
-        infoWindow: render_to_string(partial: "info_window", locals: { user: user })
       }
     end
   end
